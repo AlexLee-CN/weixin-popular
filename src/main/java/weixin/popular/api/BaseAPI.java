cbf01a25fb917a7923d03f505ad3abe2990e13bf
李家大少 <lixor@qq.com>
2015-08-06 14:04:17 +0800

    Update BaseAPI.java
    修改MEDIA_URI的默认值，参考http://mp.weixin.qq.com/wiki/14/7e6c03263063f4813141c3e17dd4350a.html

--- src/main/java/weixin/popular/api/BaseAPI.java
+++ src/main/java/weixin/popular/api/BaseAPI.java
@@ -7,7 +7,7 @@ import org.apache.http.message.BasicHeader;
 
 public abstract class BaseAPI {
 	protected static final String BASE_URI = "https://api.weixin.qq.com";
-	protected static final String MEDIA_URI = "http://file.api.weixin.qq.com";
+	protected static final String MEDIA_URI = "https://api.weixin.qq.com";//http://mp.weixin.qq.com/wiki/14/7e6c03263063f4813141c3e17dd4350a.html
 	protected static final String QRCODE_DOWNLOAD_URI = "https://mp.weixin.qq.com";
 	protected static final String MCH_URI = "https://api.mch.weixin.qq.com";
 	protected static final String OPEN_URI = "https://open.weixin.qq.com";
